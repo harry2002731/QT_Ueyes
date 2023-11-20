3ea722c226a4673cb611e27eaa47492005d7c094
eilsonwen <613476@qq.com>
2023-11-20 17:55:37 +0800

    新增，用以对应进程的内存

---
+++ src/SysInfoFetcher.h
@@ -0,0 +1,36 @@
+#ifndef SYSINFOFETCHER_H
+#define SYSINFOFETCHER_H
+
+
+#include <QProcess>
+#include <iostream>
+
+#include <QTime>
+#include <QTimer>
+#include <QLabel>
+#include <QFrame>
+#include <QBoxLayout>
+#include <QSettings>
+#include <QDockWidget>
+#include <QDebug>
+#include <QResizeEvent>
+#include <QPlainTextEdit>
+#include <QTableWidget>
+#include <QMessageBox>
+#include <QPointer>
+#include "ads_globals.h"
+
+
+
+#if QT_VERSION > QT_VERSION_CHECK(6, 0, 0)
+    #include <QRegularExpression>
+#endif
+
+class ADS_EXPORT SysInfoFetcher
+{
+public:
+    SysInfoFetcher();
+    QString MemFetcher(QString imageName);
+};
+
+#endif // SYSINFOFETCHER_H
