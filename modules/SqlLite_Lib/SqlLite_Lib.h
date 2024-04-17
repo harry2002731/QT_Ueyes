21fd554a3adee6e3f835653ad922305624593a49
eilsonwen <613476@qq.com>
2024-04-17 23:21:49 +0800

    新增多按钮点击查询python的分类数据

--- modules/SqlLite_Lib/SqlLite_Lib.h
+++ modules/SqlLite_Lib/SqlLite_Lib.h
@@ -1,5 +1,7 @@
+
 #ifndef SQLLITE_LIB_H
 #define SQLLITE_LIB_H
+#include <Python.h>
 
 #include "SqlLite_Lib_global.h"
 #include <iostream>
@@ -13,6 +15,7 @@
 #include <QSqlRelationalTableModel>
 #include "QtWidgets/qtableview.h"
 #include <QTableWidget>
+#include "qpushbutton.h"
 
 class SQLLITE_LIB_EXPORT SqlLite_Lib: public QObject, public DeclareInterface
 {
@@ -25,17 +28,28 @@ public:
     QSqlDatabase  connectDB(QString db_name); //链接数据库
     void getTableInfo(QSqlDatabase db, QString table_name); //获取表信息
     QWidget* createWidget(int page);
+    QWidget* createWidget2(int page);
+
     QSqlDatabase db ;
     QWidget* big_widget = new QWidget();
     QTableWidget* tableWidget = new QTableWidget;
     int countLineNum();
     void switchPage(int page);
+    void switchPage2(int page);
+
     int currentPage = 0;
     int totalPages = 100; // 假设有 10 页数据
     QString table_name;
+    PyObject* pInstanceSvmAnalyzer;
+    QString aaa = "Addictive Behavior";
+
 private slots:
     void showPreviousPage();
     void showNextPage();
+    void showPreviousPage2();
+    void showNextPage2();
+    void changeState_triggered2();
+
 };
 
 #endif // SQLLITE_LIB_H
