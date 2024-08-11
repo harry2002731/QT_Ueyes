11c528713de9bdaa5223ee9e1f3c78a08996844b
eilsonwen <613476@qq.com>
2024-08-11 21:14:26 +0800

    修改界面为拖拽流程图的界面布局

--- Ueyes/studioUeyes/WelcomeWindow.cpp
+++ Ueyes/studioUeyes/WelcomeWindow.cpp
@@ -1,28 +1,74 @@
 #include "WelcomeWindow.h"
-
+#include "Widget_Lib/FuncViewer/FuncViewerWidget.h"
 WelcomeWindow::WelcomeWindow(QWidget *parent,QString name):
     BasicWindow(parent,name)
 {
 
 }
+static std::shared_ptr<NodeDelegateModelRegistry> registerDataModels()
+{
+    auto ret = std::make_shared<NodeDelegateModelRegistry>();
+    //    ret->registerModel<ImageShowModel>();
+
+    //    ret->registerModel<ImageLoaderModel>();
+
+    //    ret->registerModel<ImageGrayModel>();
+
+    return ret;
+}
 
 //创建视频监视窗口
-ads::CDockWidget* WelcomeWindow::createLoginWidget()
+ads::CDockWidget* WelcomeWindow::createDesignWidget()
 {
+    FuncViewerWidget* aaa = new FuncViewerWidget();
     static int CameraViewerCount = 0;
-    auto w = new VideoPanel();
+
+    DataFlowModel graphModel(registerDataModels());
+
+    QMainWindow mainWin2;
+    scene = new DataFlowGraphicsScene(graphModel,mainWin2);
+    view = new GraphicsView(scene);
+    view->setAcceptDrops(true);
+    QGroupBox *groupBox = new QGroupBox("Options");
+
+    QCheckBox *cb1 = new QCheckBox("Nodes are locked");
+    QCheckBox *cb2 = new QCheckBox("Connections detachable");
+    cb2->setChecked(true);
+
+    QVBoxLayout *vbl = new QVBoxLayout;
+    vbl->addWidget(cb1);
+    vbl->addWidget(cb2);
+    vbl->addStretch();
+    groupBox->setLayout(vbl);
+
+    window->addWidget(aaa);
+    window->addWidget(view);
+    window->setStretchFactor(1, 4);
+
+
+
+    QObject::connect(cb1, &QCheckBox::stateChanged, [&graphModel](int state) {
+        graphModel.setNodesLocked(state == Qt::Checked);
+    });
+
+    QObject::connect(cb2, &QCheckBox::stateChanged, [&graphModel](int state) {
+        graphModel.setDetachPossible(state == Qt::Checked);
+    });
+
+    auto w = window;
     ads::CDockWidget* DockWidget = new ads::CDockWidget(QString("Table %1").arg(CameraViewerCount++));
     DockWidget->setWidget(w);
     DockWidget->setMinimumSize(100,100);
 
     DockWidget->setMinimumSizeHintMode(ads::CDockWidget::MinimumSizeHintFromDockWidgetMinimumSize);
     return DockWidget;
+
 }
 
 void WelcomeWindow::createContent()
 {
-    auto CameraViewer = createCameraViewerWidget();
-    this->addDockWidget(ads::LeftDockWidgetArea, CameraViewer);
-    auto aaa = createEditorWidget();
-    this->addDockWidget(ads::RightDockWidgetArea, aaa);
+    auto DesignerViewer = createDesignWidget();
+    this->addDockWidget(ads::LeftDockWidgetArea, DesignerViewer);
+//    auto aaa = createEditorWidget();
+//    this->addDockWidget(ads::RightDockWidgetArea, aaa);
 }
--- Ueyes/studioUeyes/WelcomeWindow.h
+++ Ueyes/studioUeyes/WelcomeWindow.h
@@ -4,13 +4,37 @@
 #include <QString>
 #include <QWidget>
 
+#include <QtNodes/DataFlowGraphModel>
+#include <QtNodes/DataFlowGraphicsScene>
+#include <QtNodes/GraphicsView>
+#include <QtNodes/NodeData>
+#include <QtNodes/NodeDelegateModelRegistry>
+
+#include <QtGui/QScreen>
+#include <QtWidgets/QApplication>
+#include "DataFlowModel.h"
+#include "qboxlayout.h"
+#include "qcheckbox.h"
+#include "qgroupbox.h"
+#include <QSplitter>
+
+using QtNodes::ConnectionStyle;
+using QtNodes::DataFlowGraphicsScene;
+using QtNodes::DataFlowGraphModel;
+using QtNodes::GraphicsView;
+using QtNodes::NodeDelegateModelRegistry;
 class WelcomeWindow : public BasicWindow
 {
     Q_OBJECT
 public:
     WelcomeWindow(QWidget *parent,QString name);
     void createContent() override;
-    virtual ads::CDockWidget* createLoginWidget();
+    virtual ads::CDockWidget* createDesignWidget();
+    DataFlowGraphicsScene *scene;
+    GraphicsView  *view;
+    QSplitter * window = new QSplitter(Qt::Horizontal);
+
+
 };
 
 #endif // WELCOMEWINDOW_H
